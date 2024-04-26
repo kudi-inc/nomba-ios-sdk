a7870483f549f0a3d85617dd50ae1cc2a0419660
Bezaleel Ashefor <knightbenax@gmail.com>
2024-04-26 16:32:04 +0100

    Birth Head

---
+++ Octance/Octance/Octance.h
@@ -0,0 +1,18 @@
+//
+//  Octance.h
+//  Octance
+//
+//  Created by Bezaleel Ashefor on 26/04/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Octance.
+FOUNDATION_EXPORT double OctanceVersionNumber;
+
+//! Project version string for Octance.
+FOUNDATION_EXPORT const unsigned char OctanceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Octance/PublicHeader.h>
+
+

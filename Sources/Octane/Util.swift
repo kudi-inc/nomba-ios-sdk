6137d38f3365f758775a9ccf736af925b82dd4d4
Bezaleel Ashefor <knightbenax@gmail.com>
2024-06-15 13:24:13 +0100

    Update Util.swift

--- Sources/Octane/Util.swift
+++ Sources/Octane/Util.swift
@@ -13,6 +13,7 @@ class Util {
     static func getDrop(message: String) -> Drop {
         return Drop(
             title: message,
+            titleNumberOfLines: 2,
             action: .init {
                 Drops.hideCurrent()
             }

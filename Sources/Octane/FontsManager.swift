541040969cd44064dae06e26683312b9fdca09a7
Damilola Adeniyi <adeniyi.damilola@nomba.com>
2025-04-14 09:36:31 +0100

    update register font  method

--- Sources/Octane/FontsManager.swift
+++ Sources/Octane/FontsManager.swift
@@ -26,12 +26,13 @@ class FontsManager {
     }
 
     func registerFont(named name: String) throws {
-       guard let asset = NSDataAsset(name: "Fonts/\(name)", bundle: Bundle.module),
-          let provider = CGDataProvider(data: asset.data as NSData),
-          let font = CGFont(provider),
-          CTFontManagerRegisterGraphicsFont(font, nil) else {
-        throw FontError.failedToRegisterFont
-       }
+        guard let asset = NSDataAsset(name: "Fonts/\(name)", bundle: Bundle.module),
+              let provider = CGDataProvider(data: asset.data as NSData),
+              let font = CGFont(provider),
+              CTFontManagerRegisterGraphicsFont(font, nil) else {
+            throw FontError.failedToRegisterFont
+        }
     }
+
     
 }
